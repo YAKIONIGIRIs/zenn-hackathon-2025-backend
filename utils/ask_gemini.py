4f615cb51b445c1947055e2d8807766cb44e1e72
tsmiyamoto <miya@sizrae.jp>
2025-01-31 16:46:06 +0900

    fix: remove non-existent imports

--- utils/ask_gemini.py
+++ utils/ask_gemini.py
@@ -2,10 +2,11 @@ import json
 import sys
 
 import vertexai
-from vertexai.generative_models import GenerationConfig, GenerativeModel
+from vertexai.generative_models import GenerativeModel
 
 project_id = "ykongrs-zenn-hackathon-2025"
 location = "us-central1"
+
 
 class GeminiHelper:
     def __init__(self):
@@ -26,12 +27,9 @@ class GeminiHelper:
             "type": "array",
             "items": {
                 "type": "object",
-                "properties": {
-                    "word": {"type": "string"},
-                    "description": {"type": "string"}
-                },
-                "required": ["word", "description"]
-            }
+                "properties": {"word": {"type": "string"}, "description": {"type": "string"}},
+                "required": ["word", "description"],
+            },
         }
         ask_sentence = f"""
             下記文章には補足が必要な単語がある可能性があります。
@@ -42,22 +40,20 @@ class GeminiHelper:
         """
 
         # This function should call the Gemini API to get the words that need additional information
-        generation_config = GenerationConfig(
-            response_mime_type="application/json",
-            response_schema=response_schema
-        )
+        generation_config = GenerationConfig(response_mime_type="application/json", response_schema=response_schema)
         response = self.model.generate_content(ask_sentence, generation_config=generation_config)
         print(response.text)
         # Assuming the response is a JSON string
         response_json = json.loads(response.text)
         # Extract the words that need additional information
         for word_data in response_json:
-            if (word_data["word"] in self.word_list):
+            if word_data["word"] in self.word_list:
                 pass
             else:
                 return_data.append(word_data)
                 self.word_list.append(word_data["word"])
         return return_data
+
 
 def main():
     input_data = json.load(sys.stdin)
@@ -68,6 +64,7 @@ def main():
     result = gemini_helper.ask_gemini()
 
     print(json.dumps(result, indent=2, ensure_ascii=False))
+
 
 if __name__ == "__main__":
     main()
