297ef31971f76936798be3d0f81a8a56ecc82687
toryo-bobo <toryo1323@gmail.com>
2025-02-01 14:51:39 +0900

    クラウド構成の変更のためFlaskパスのみ作成

--- app.py
+++ app.py
@@ -48,46 +48,82 @@ def hello() -> str:
 
     return "Hello, World!"
 
-@app.route("/start_conversation", methods=["POST"])
-def start_conversation() -> str:
+@app.route("/start_meet", methods=["POST"])
+def start_meet() -> str:
     """
-    start_conversation: Start a new conversation with a new session_id
-    :param: session_id: str
+    start_meet: Start a new meet with a new userName
+    :param: meetId: str
     :return: response: dict
     """
     # Get JSON data from POST request
     chatdata_json = request.get_json()
     logger.info(f"Received data: {chatdata_json}")
 
+    # Start a new meet with a new meetId
     try:
-        # Check if session_id exists in gemini_helper
-        if (not gemini_helper.keys(chatdata_json["session_id"])):
-            gemini_helper[chatdata_json["session_id"]] = GeminiHelper()
+        if ("meetId" in chatdata_json):
+            # Return the meetId as the userName
             jsondata_start = {
                 "result": True,
-                "message": "Conversation started"
+                "message": ""
             }
         else:
-            # If session_id exists, return error message
             jsondata_start = {
                 "result": False,
-                "message": "session_id already exists"
+                "message": "missing required keys"
             }
-    except KeyError:
-        # If session_id does not exist, return error message
+    except Exception as e:
+        logger.error(f"Error starting meet: {e}")
         jsondata_start = {
             "result": False,
-            "message": "keyError"
+            "message": "error starting meet"
         }
 
     response = jsonify(jsondata_start)
     return response
 
+@app.route("/save_transcript", methods=["POST"])
+def save_transcript() -> str:
+    """
+    save_transcript: Save transcript data to gemini_helper
+    :param: meetId: str
+    :param: userName: str
+    :param: transcript: str
+    :return: response: dict
+    """
+    # Get JSON data from POST request
+    chatdata_json = request.get_json()
+    logger.info(f"Received data: {chatdata_json}")
+
+    # Store the text date to the Firestrore
+    try:
+        # Check if the json data has the required keys
+        if ("meetId" in chatdata_json and "userName" in chatdata_json and "transcript" in chatdata_json):
+            jsondata_save = {
+                "result": True,
+                "message": ""
+            }
+        else:
+            jsondata_save = {
+                "result": False,
+                "message": "missing required keys"
+            }
+    except Exception as e:
+        logger.error(f"Error saving transcript: {e}")
+        jsondata_save = {
+            "result": False,
+            "message": "error saving transcript"
+        }
+
+    response = jsonify(jsondata_save)
+    return response
+
+
 @app.route("/get_supplement", methods=["POST"])
 def get_supplement() -> str:
     """
     get_supplement: Get supplement data from Gemini API
-    :param: session_id: str
+    :param: userName: str
     :param: text: str
     :return: response: dict
     """
@@ -96,35 +132,40 @@ def get_supplement() -> str:
     logger.info(f"Received data: {chatdata_json}")
 
     try:
-        # Check if session_id exists in gemini_helper
-        if (not gemini_helper.keys(chatdata_json["session_id"])):        
-            # If session_id exists, add text to gemini_helper and ask gemini
-            gemini_helper[chatdata_json["session_id"]].add_text(chatdata_json["text"])
-            jsondata_supplement = gemini_helper[chatdata_json["session_id"]].ask_gemini()
-            jsondata_supplement["result"] = True  
+        # Check if userName exists in gemini_helper
+        if ("meetId" in chatdata_json and "userName" in chatdata_json and "role" in chatdata_json):
+            testdata = {
+                "word": "テスト文章",
+                "description": "テスト補足"
+            }
+            jsondata_supplement = {
+                "supplement": [testdata],
+                "result": True,
+                "message": ""
+            }
         else:
-            # If session_id does not exist, return error message
+            # If userName does not exist, return error message
             jsondata_supplement = {
                 "supplement": [],
                 "result": False,
-                "message": "No session_id found"
+                "message": "missing required keys"
             }
-    except KeyError:
-        # If session_id does not exist, return error message
+    except Exception as e:
+        logger.error(f"Error getting supplement: {e}")
         jsondata_supplement = {
             "supplement": [],
             "result": False,
-            "message": "keyError"
+            "message": "error getting supplement"
         }
 
     response = jsonify(jsondata_supplement)
     return response
 
-@app.route("/end_conversation", methods=["POST"])
-def end_conversation() -> str:
+@app.route("/end_meet", methods=["POST"])
+def end_meet() -> str:
     """
-    end_conversation: End conversation with session_id
-    :param: session_id: str
+    end_meet: End meet with meetId
+    :param: meetId: str
     :return: response: dict
     """
     # Get JSON data from POST request
@@ -132,25 +173,23 @@ def end_conversation() -> str:
     logger.info(f"Received data: {chatdata_json}")
 
     try:
-        # Check if session_id exists in gemini_helper
-        if (not gemini_helper.keys(chatdata_json["session_id"])):        
-            # If session_id exists, end conversation
-            gemini_helper.pop(chatdata_json["session_id"])
+        # Check if userName exists in gemini_helper
+        if ("meetId" in chatdata_json):
             jsondata_end = {
-                "message": "Conversation ended",
-                "result": True
+                "result": True,
+                "message": ""
             }
         else:
-            # If session_id does not exist, return error message
+            # If userName does not exist, return error message
             jsondata_end = {
-                "message": "No session_id found",
-                "result": False
+                "result": False,
+                "message": "missing required keys"
             }
-    except KeyError:
-        # If session_id does not exist, return error message
+    except Exception as e:
+        logger.error(f"Error ending meet: {e}")
         jsondata_end = {
-            "message": "keyError",
-            "result": False
+            "result": False,
+            "message": "error ending meet"
         }
 
     response = jsonify(jsondata_end)
@@ -160,6 +199,7 @@ def shutdown_handler(signal_int: int, frame: FrameType) -> None:
     logger.info(f"Caught Signal {signal.strsignal(signal_int)}")
 
     from utils.logging import flush
+from google.cloud import firestore
 
     flush()
 
